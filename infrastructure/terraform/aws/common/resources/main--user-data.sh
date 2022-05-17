e1acb5ab15b4992e8e9c60ec4faee3a4e955f5f3
Stroganov <petr.stroganov@gmail.com>
2022-05-17 21:56:54 +0900

    Add sheebang

--- infrastructure/terraform/aws/common/resources/main--user-data.sh
+++ infrastructure/terraform/aws/common/resources/main--user-data.sh
@@ -1,8 +1,7 @@
+#!/bin/bash
 sudo yum install docker
 sudo usermod -a -G docker ec2-user
 id ec2-user
 sudo pip3 install docker-compose
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
-
-
