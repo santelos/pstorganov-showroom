76aad06ba6b7389a9c674df4cd5803727c0073ae
Petr Stroganov <petr.stroganov@gmail.com>
2022-05-17 22:57:15 +0900

    Add mkdir (#22)

--- infrastructure/terraform/aws/common/resources/main--user-data.sh
+++ infrastructure/terraform/aws/common/resources/main--user-data.sh
@@ -5,3 +5,6 @@ sudo usermod -a -G docker ec2-user
 sudo pip3 --no-input install docker-compose
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
+
+mkdir ~/infrastructure
+mkdir ~/main
