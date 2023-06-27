ae4a4122981a59a6b4570a7156ece0006857d530
Petr Stroganov <petr.stroganov@gmail.com>
2023-06-27 23:29:23 +0900

    Deployment of the services to local k8s (#40)

--- services/main/token-mediating-backend/src/main/kotlin/ru/stroganov/oauth2/tokenmediatingbackend/Environment.kt
+++ services/main/token-mediating-backend/src/main/kotlin/ru/stroganov/oauth2/tokenmediatingbackend/Environment.kt
@@ -2,6 +2,7 @@ package ru.stroganov.oauth2.tokenmediatingbackend
 
 val OAUTH2__CALLBACK_URL: String = System.getenv("OAUTH2__CALLBACK_URL")
 val OAUTH2__PROVIDER_URL: String = System.getenv("OAUTH2__PROVIDER_URL")
+val OAUTH2__PROVIDER_INTERNAL_URL: String = System.getenv("OAUTH2__PROVIDER_INTERNAL_URL")
 val OAUTH2__CLIENT_ID: String = System.getenv("OAUTH2__CLIENT_ID")
 val OAUTH2__CLIENT_SECRET: String = System.getenv("OAUTH2__CLIENT_SECRET")
 
